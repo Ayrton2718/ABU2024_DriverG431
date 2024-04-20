20c6ddfab26f8485e7c411837cc470dc791e4ed1
Ayrton2718 <matsushita.sena@gmail.com>
2024-04-20 15:36:36 +0900

    Interrupt.

--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -22,22 +22,26 @@ static bool UserTask_checksum(uint8_t low_byte, uint8_t high_byte);
 
 static bool g_rst_flg;
 
-static count_t g_count_reg;
+volatile static count_t g_count_reg;
 static uint8_t  g_id;
 
 static CSTimer_t g_tim;
+static CSTimer_t g_send_tim;
 static CSTimer_t g_timeout;
 
+uint8_t g_rx_data[2] = {0};
 
 void UserTask_setup(void)
 {
 	g_id = AMT212B_ID;
+
     g_count_reg.rot_count = 0;
     g_count_reg.angle = 0;
 
     g_rst_flg = false;
 
     CSTimer_start(&g_tim);
+    CSTimer_start(&g_send_tim);
     CSTimer_start(&g_timeout);
 
     CSIo_bind(CSType_appid_AMT212B, UserTask_canCallback, UserTask_resetCallback);
@@ -47,47 +51,34 @@ void UserTask_setup(void)
 void UserTask_loop(void)
 {
     uint32_t us = CSTimer_getUs(g_tim);
-    if(1000 < us)
+    if(300 < us)
     {
         CSTimer_start(&g_tim);
 
-        uint8_t rx_data[2] = {0};
-        if(HAL_UART_Transmit(RS485_HUART, &g_id, 1, 10) == HAL_OK)
+        g_rx_data[0] = 0;
+        g_rx_data[1] = 0;
+        if(HAL_UART_Receive_IT(RS485_HUART, g_rx_data, 2) != HAL_OK)
         {
-            if(HAL_UART_Receive(RS485_HUART, rx_data, 2, 10) == HAL_OK)
-			{
-				if(UserTask_checksum(rx_data[0], rx_data[1]))
-				{
-					uint16_t last_angle = g_count_reg.angle;
-
-					g_count_reg.angle = ((rx_data[1] & 0x3F) << 8 | rx_data[0]);
-
-					if(last_angle < g_count_reg.angle)
-					{
-						if(8192 < (g_count_reg.angle - last_angle))
-						{
-							g_count_reg.rot_count--;
-						}
-					}else{
-						if(8192 < (last_angle - g_count_reg.angle))
-						{
-							g_count_reg.rot_count++;
-						}
-					}
-                    CSTimer_start(&g_timeout);
-				}else{
-                    CSLed_err();
-                }
-			}else{
-				CSLed_err();
-			}
-        }else{
-			CSLed_err();
+            CSLed_err();
         }
+        if(HAL_UART_Transmit(RS485_HUART, &g_id, 1, 10) != HAL_OK)
+        {
+            CSLed_err();
+        }
+    }
+
+    if(1 < CSTimer_getMs(g_send_tim))
+    {
+        CSTimer_start(&g_send_tim);
 
         if(CSTimer_getMs(g_timeout) < 100)
         {
-            CSIo_sendUser(CSReg_0, (const uint8_t*)&g_count_reg, sizeof(count_t));
+            count_t reg;
+            reg.angle = g_count_reg.angle;
+            reg.rot_count = g_count_reg.rot_count;
+            CSIo_sendUser(CSReg_0, (const uint8_t*)&reg, sizeof(count_t));
+        }else{
+            CSLed_err();
         }
     }
 
@@ -124,4 +115,35 @@ static bool UserTask_checksum(uint8_t low_byte, uint8_t high_byte) {
     bool k1 = !(h(5) ^ h(3) ^ h(1) ^ l(7) ^ l(5) ^ l(3) ^ l(1));
     bool k0 = !(h(4) ^ h(2) ^ h(0) ^ l(6) ^ l(4) ^ l(2) ^ l(0));
     return (k1 == h(7)) && (k0 == h(6));
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance == RS485_HUART->Instance)
+	{
+        if(UserTask_checksum(g_rx_data[0], g_rx_data[1]))
+        {
+            uint16_t last_angle = g_count_reg.angle;
+
+            uint16_t now_angle = ((g_rx_data[1] & 0x3F) << 8 | g_rx_data[0]);
+
+            if(last_angle < now_angle)
+            {
+                if(8192 < (now_angle - last_angle))
+                {
+                    g_count_reg.rot_count--;
+                }
+            }else{
+                if(8192 < (last_angle - now_angle))
+                {
+                    g_count_reg.rot_count++;
+                }
+            }
+
+            g_count_reg.angle = now_angle;
+            CSTimer_start(&g_timeout);
+        }else{
+            CSLed_err();
+        }
+	}
 }
