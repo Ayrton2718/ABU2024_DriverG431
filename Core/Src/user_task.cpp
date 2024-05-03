c57f175495ec7e5415f92266762af0b7019d5e02
Ayrton2718 <matsushita.sena@gmail.com>
2024-05-03 17:30:18 +0900

    Specification change

--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -18,11 +18,13 @@ static CSType_bool_t UserTask_canCallback(CSReg_t reg, const uint8_t* data, size
 static void UserTask_resetCallback(void);
 static void UserTask_timerCallback(void);
 
+static inline void get_colors(void);
+
 static colors_t g_colors_reg;
 static uint16_t g_err_count[3];
 
 static bool g_rst_flg;
-static CSTimer_t g_sensor_tim;
+static CSTimer_t g_elapsed_tim;
 static CSTimer_t g_send_tim;
 
 
@@ -38,6 +40,8 @@ void UserTask_setup(void)
     g_err_count[0] = 0;
     g_err_count[1] = 0;
     g_err_count[2] = 0;
+
+    CSTimer_start(&g_elapsed_tim);
 
     for(uint16_t i = 0; i < 200; i++)
     {
@@ -55,7 +59,6 @@ void UserTask_setup(void)
 
     g_rst_flg = false;
     
-    CSTimer_start(&g_sensor_tim);
     CSTimer_start(&g_send_tim);
     CSIo_bind(CSType_appid_COLOR, UserTask_canCallback, UserTask_resetCallback);
     CSTimer_bind(UserTask_timerCallback);
@@ -63,59 +66,33 @@ void UserTask_setup(void)
 
 void UserTask_loop(void)
 {
-    if(HAL_GPIO_ReadPin(SW2_GPIO_Port, SW2_Pin) == GPIO_PIN_RESET){
-        uint32_t ms = CSTimer_getMs(g_sensor_tim);
-        if(24 < ms)
-        {
-            CSTimer_start(&g_sensor_tim);
-
-            int16_t red, blue, clear;
-            if(veml::getRed(&red) == HAL_OK){
-                g_colors_reg.red = red;
-                g_err_count[0] = 0;
-            }else{
-                g_err_count[0]++;
-                if(4 < g_err_count[0]){
-                    CSLed_err();
-                }
-            }
-
-            if(veml::getBlue(&blue) == HAL_OK){
-                g_colors_reg.blue = blue;
-                g_err_count[1] = 0;
-            }else{
-                g_err_count[1]++;
-                if(4 < g_err_count[1]){
-                    CSLed_err();
-                }
-            }
-
-            if(veml::getClear(&clear) == HAL_OK){
-                g_colors_reg.clear = clear;
-                g_err_count[2] = 0;
-            }else{
-                g_err_count[2]++;
-                if(4 < g_err_count[2]){
-                    CSLed_err();
-                }
-            }
-
-            if(20 * 4 < g_err_count[0] + g_err_count[1] + g_err_count[2]){
-                veml::begin();
-            }
-        }
-    }
-
     uint32_t us = CSTimer_getUs(g_send_tim);
-    if(900 < us)
+    if(24000 < us)
     {
         CSTimer_start(&g_send_tim);
 
-        g_colors_reg.elapsed_ms = CSTimer_getMs(g_sensor_tim);
+        if(HAL_GPIO_ReadPin(SW2_GPIO_Port, SW2_Pin) == GPIO_PIN_RESET){
+            get_colors();
+
+            CSTimer_start(&g_elapsed_tim);
+            g_colors_reg.elapsed_ms = 0;
+        }else{
+            g_colors_reg.elapsed_ms = CSTimer_getMs(g_elapsed_tim);
+        }
+
         CSIo_sendUser(CSReg_0, (const uint8_t*)&g_colors_reg, sizeof(colors_t));
+
+
+        if(20 * 4 < g_err_count[0] + g_err_count[1] + g_err_count[2]){
+            veml::begin();
+        }
     }
 
     if(g_rst_flg){
+        g_colors_reg.red = 0;
+        g_colors_reg.blue = 0;
+        g_colors_reg.clear = 0;
+        
         g_rst_flg = 0;
     }
 }
@@ -137,4 +114,38 @@ static CSType_bool_t UserTask_canCallback(CSReg_t reg, const uint8_t* data, size
 static void UserTask_resetCallback(void)
 {
 	g_rst_flg = 1;
+}
+
+static void get_colors(void)
+{
+    int16_t red, blue, clear;
+    if(veml::getRed(&red) == HAL_OK){
+        g_colors_reg.red = red;
+        g_err_count[0] = 0;
+    }else{
+        g_err_count[0]++;
+        if(4 < g_err_count[0]){
+            CSLed_err();
+        }
+    }
+
+    if(veml::getBlue(&blue) == HAL_OK){
+        g_colors_reg.blue = blue;
+        g_err_count[1] = 0;
+    }else{
+        g_err_count[1]++;
+        if(4 < g_err_count[1]){
+            CSLed_err();
+        }
+    }
+
+    if(veml::getClear(&clear) == HAL_OK){
+        g_colors_reg.clear = clear;
+        g_err_count[2] = 0;
+    }else{
+        g_err_count[2]++;
+        if(4 < g_err_count[2]){
+            CSLed_err();
+        }
+    }
 }
