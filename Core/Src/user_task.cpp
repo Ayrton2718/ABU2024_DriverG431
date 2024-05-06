1b954d4f7f6e3fced209938c4ae2e4b129ecdca3
Ayrton2718 <matsushita.sena@gmail.com>
2024-05-07 01:17:13 +0900

    Add switch.

--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -1,6 +1,7 @@
 #include "user_task.h"
 #include "can_smbus/can_smbus.hpp"
 
+#include <array>
 #include "veml3328.hpp"
 
 extern "C" {
@@ -12,6 +13,29 @@ typedef struct{
     uint16_t elapsed_ms;
 }__attribute__((__packed__)) colors_t;
 
+typedef struct{
+    uint8_t list1;
+}__attribute__((__packed__)) switch_t;
+
+inline bool get_sw(uint8_t* list, int index){
+    if (index < 0 || index > 7) {
+        return false;
+    }
+    return (*list >> index) & 1;
+}
+
+// 指定されたインデックスにbool値を設定する関数
+inline void set_sw(uint8_t* list, int index, bool value) {
+    if (index < 0 || index > 7) {
+        return;
+    }
+    if (value) {
+        *list |= (1 << index); // 指定されたビットを1に設定
+    } else {
+        *list &= ~(1 << index); // 指定されたビットを0に設定
+    }
+}
+
 }
 
 static CSType_bool_t UserTask_canCallback(CSReg_t reg, const uint8_t* data, size_t len);
@@ -20,12 +44,15 @@ static void UserTask_timerCallback(void);
 
 static inline void get_colors(void);
 
+static switch_t g_sw_reg;
 static colors_t g_colors_reg;
 static uint16_t g_err_count[3];
 
 static bool g_rst_flg;
 static CSTimer_t g_elapsed_tim;
 static CSTimer_t g_send_tim;
+
+static std::array<std::pair<GPIO_TypeDef*, uint16_t >, 2> g_sw_pins;
 
 
 void UserTask_setup(void)
@@ -40,6 +67,9 @@ void UserTask_setup(void)
     g_err_count[0] = 0;
     g_err_count[1] = 0;
     g_err_count[2] = 0;
+
+    g_sw_pins[0] = std::make_pair(SW1_GPIO_Port, SW1_Pin);
+    g_sw_pins[1] = std::make_pair(SW2_GPIO_Port, SW2_Pin);
 
     CSTimer_start(&g_elapsed_tim);
 
@@ -82,6 +112,16 @@ void UserTask_loop(void)
 
         CSIo_sendUser(CSReg_0, (const uint8_t*)&g_colors_reg, sizeof(colors_t));
 
+		// Switches
+        for(size_t i = 0; i < g_sw_pins.size(); i++)
+		{
+			if(HAL_GPIO_ReadPin(g_sw_pins[i].first, g_sw_pins[i].second) == GPIO_PIN_SET){
+				set_sw(&g_sw_reg.list1, i, false);
+			}else{
+				set_sw(&g_sw_reg.list1, i, true);
+			}
+		}
+		CSIo_sendUser(CSReg_1, (const uint8_t*)&g_sw_reg, sizeof(switch_t));
 
         if(20 * 4 < g_err_count[0] + g_err_count[1] + g_err_count[2]){
             veml::begin();
