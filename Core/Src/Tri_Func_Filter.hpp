c80dff678438ea23868ef4d1e4138b1562cc9ba7
banpeiyu <hiro.chun.manmaru@gmail.com>
2024-05-23 04:15:01 +0900

    R2専用光電のバックアップ

---
+++ Core/Src/Tri_Func_Filter.hpp
@@ -0,0 +1,117 @@
+/**
+ * @file Tri_Func_Filter.hpp
+ * @brief 三角関数を用いた、ステップ入力をなめらかにするフィルタ
+ * @version 0.1
+ * @date 2024-03-23
+ * 
+ * 
+ */
+
+#pragma once
+
+#include <math.h>
+
+namespace tut_mec_filter{
+
+class Cos_Filter{
+
+private:
+    float _reach_time;
+    float _integral_rad;
+    float _const_cycle_time;
+    float _cycle_time;
+    float _hold_data;
+    float _target_data;
+    float _bef_target_data;
+    float _data_delta;
+    float _addition_data;
+    float _output;
+    bool _is_init;
+
+public:
+    Cos_Filter(){ }
+
+    void init(float REACH_TIME, float CONST_CYCLE_TIME){
+        _reach_time = REACH_TIME;
+        _const_cycle_time = CONST_CYCLE_TIME;
+        _cycle_time = _const_cycle_time;
+        _is_init = true;
+
+        this->Reset();
+    }
+
+    void Set_Base(float BASE){
+        this->Reset();
+        _target_data = BASE;
+        _bef_target_data = BASE;
+        _hold_data = BASE;
+        _output = BASE;
+    }
+
+    void Set_Reach_Time(float TIME){
+        _reach_time = TIME;
+    }
+
+    void Set_Data(float TARGET){
+        _target_data = TARGET;
+    }
+
+    void Update(float CYCLE_TIME = -1){
+        if(!_is_init){
+            return;
+        }
+
+        if(CYCLE_TIME < 0){
+            _cycle_time = _const_cycle_time;
+        }
+        else{
+            _cycle_time = CYCLE_TIME;
+        }
+
+        if(_bef_target_data != _target_data){
+            _hold_data = _hold_data + _addition_data;
+            _data_delta = _target_data - _hold_data;
+            _integral_rad = 0;
+            _bef_target_data = _target_data;
+        }
+
+        if(_integral_rad < M_PI){
+            _addition_data = _data_delta * ((1-cos(_integral_rad)) / 2.0);
+            _output = _hold_data + _addition_data;
+            if(_reach_time <= 0){
+                _integral_rad = M_PI;
+            }
+            else{
+                _integral_rad += (M_PI / _reach_time) * _cycle_time;
+            }
+        }
+        else{
+            _addition_data = _data_delta;
+            _output = _hold_data + _addition_data;
+        }
+    }
+
+    float Output(){ return _output; }
+
+    float Cal_Data(float TARGET, float CYCLE_TIME = -1){
+        this->Set_Data(TARGET);
+        this->Update(CYCLE_TIME);
+        return this->Output();
+    }
+
+    void Reset(){
+        _target_data = 0;
+        _bef_target_data = 0;
+        _hold_data = 0;
+        _data_delta = 0;
+        _addition_data = 0;
+        _integral_rad = 0;
+        _output = 0;
+    }
+
+
+
+};
+
+
+}
--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -1,5 +1,6 @@
 #include "user_task.h"
 #include "can_smbus/can_smbus.hpp"
+#include "Tri_Func_Filter.hpp"
 
 #include <array>
 
@@ -8,6 +9,13 @@ extern "C" {
 typedef struct{
     uint8_t list1;
 }__attribute__((__packed__)) switch_t;
+
+typedef struct{
+	uint16_t count1;
+	uint16_t count2;
+	uint16_t count3;
+	uint16_t count4;
+}__attribute__((__packed__)) count_t;
 
 
 inline bool get_sw(uint8_t* list, int index){
@@ -38,7 +46,17 @@ static switch_t g_sw_reg;
 static bool g_rst_flg;
 static CSTimer_t g_tim;
 
+static count_t g_count;
+
 static std::array<std::pair<GPIO_TypeDef*, uint16_t >, 4> g_sw_pins;
+
+static std::array<tut_mec_filter::Cos_Filter, 4> g_sw_filter;
+static std::array<bool, 4> g_sw_data;
+static std::array<bool, 4> g_sw_click;
+static std::array<uint16_t, 4> g_sw_click_counter;
+const float ttl_h = 0.5;
+const float ttl_l = 0.5;
+const float cycle_time = 0.001;
 
 void UserTask_setup(void)
 {
@@ -52,14 +70,21 @@ void UserTask_setup(void)
     CSTimer_start(&g_tim);
     CSIo_bind(CSType_appid_SWITCH, UserTask_canCallback, UserTask_resetCallback);
     CSTimer_bind(UserTask_timerCallback);
+
+    for(size_t i; i < g_sw_data.size(); i++){
+    	g_sw_data[i] = false;
+    	g_sw_click[i] = false;
+    	g_sw_click_counter[i] = 0;
+    }
 }
 
 void UserTask_loop(void)
 {
     uint32_t us = CSTimer_getUs(g_tim);
-    if(10000 < us)
+    if(1000 < us)
     {
         CSTimer_start(&g_tim);
+        float sec = us * 0.000001;
 
         for(size_t i = 0; i < g_sw_pins.size(); i++)
         {
@@ -68,9 +93,35 @@ void UserTask_loop(void)
             }else{
                 set_sw(&g_sw_reg.list1, i, true);
             }
+
+            // ここからR2専用
+            float filter_output = g_sw_filter[i].Cal_Data((float)get_sw(&g_sw_reg.list1, i), sec);
+            if(ttl_h < filter_output && !g_sw_data[i]){
+            	g_sw_data[i] = true;
+            	g_sw_click[i] = true;
+            }
+            else if(filter_output < ttl_l && g_sw_data[i]){
+            	g_sw_data[i] = false;
+            	g_sw_click[i] = false;
+            }
+            else{
+            	g_sw_click[i] = false;
+            }
+
+            if(g_sw_click[i]){
+            	g_sw_click_counter[i]++;
+            	g_sw_click[i] = false;
+            }
+
         }
 
+        g_count.count1 = g_sw_click_counter[0];
+        g_count.count2 = g_sw_click_counter[1];
+        g_count.count3 = g_sw_click_counter[2];
+        g_count.count4 = g_sw_click_counter[3];
+
 		CSIo_sendUser(CSReg_0, (const uint8_t*)&g_sw_reg, sizeof(switch_t));
+		CSIo_sendUser(CSReg_1, (const uint8_t*)&g_count, sizeof(count_t));
     }
 
     if(g_rst_flg)
