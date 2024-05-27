1a3035fac4eaf16954ebb8ff82191028cfdc946f
Ayrton2718 <matsushita.sena@gmail.com>
2024-05-27 13:48:36 +0900

    Add checksum and filt vel.

--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -2,15 +2,22 @@
 #include "can_smbus/can_smbus.hpp"
 
 #include "tim.h"
+#include "math.h"
 
 #define ENC_TIM ((&htim2))
 
 extern "C" {
 
 typedef struct{
-    int32_t count;
-    int16_t pulse_ms; // pulse per milli second
+    int32_t count; //4
+    int16_t pulse_ms; // pulse per milli second //2
+    uint8_t checksum;
 }__attribute__((__packed__)) count_t;
+
+uint8_t calc_checksum(const count_t* count){
+    const uint8_t* data = (const uint8_t*)count;
+    return data[0] + data[1] + data[2] + data[3] + data[4] + data[5];
+}
 
 }
 
@@ -21,6 +28,8 @@ static void UserTask_timerCallback(void);
 static int32_t g_befor_count;
 static count_t g_count_reg;
 
+static float g_filt_pulse_ms = 0;
+
 static bool g_rst_flg;
 static CSTimer_t g_tim;
 
@@ -29,6 +38,8 @@ void UserTask_setup(void)
     HAL_TIM_Encoder_Start(ENC_TIM, TIM_CHANNEL_ALL);
     __HAL_TIM_SET_COUNTER(ENC_TIM, 0);
     g_befor_count = 0;
+
+    g_filt_pulse_ms = 0;
 
     g_rst_flg = false;
 
@@ -44,7 +55,9 @@ void UserTask_loop(void)
     {
         int32_t count = __HAL_TIM_GET_COUNTER(ENC_TIM);
         g_count_reg.count = count;
-        g_count_reg.pulse_ms = count - g_befor_count;
+        g_filt_pulse_ms = (count - g_befor_count) * 0.1 + g_filt_pulse_ms * 0.9;
+        g_count_reg.pulse_ms = (int16_t)roundf(g_filt_pulse_ms * 100);
+        g_count_reg.checksum = calc_checksum(&g_count_reg);
         g_befor_count = count;
         CSIo_sendUser(CSReg_0, (const uint8_t*)&g_count_reg, sizeof(count_t));
         CSTimer_start(&g_tim);
