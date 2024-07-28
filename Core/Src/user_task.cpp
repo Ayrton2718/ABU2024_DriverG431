7c749f87277d69735e8c3b0c7205c0b6ad84e71d
Ayrton2718 <matsushita.sena@gmail.com>
2024-07-29 01:19:31 +0900

    Update led.

--- Core/Src/user_task.cpp
+++ Core/Src/user_task.cpp
@@ -4,7 +4,18 @@
 
 #include <array>
 
-#define LED_N (7)
+#define R1
+//#define R2
+
+#ifdef R1
+#define LED1_N (6)
+#define LED2_N (6)
+#define LED_BRIGHT_RATE (0.5)
+#elif R2
+#define LED1_N (7)
+#define LED2_N (0)
+#define LED_BRIGHT_RATE (1.0)
+#endif /*R2*/
 
 extern "C" {
 
@@ -12,20 +23,27 @@ typedef struct{
     uint8_t r;
     uint8_t g;
     uint8_t b;
-}__attribute__((__packed__)) rgb_t;
+    uint8_t hz; // hz * 10
+}__attribute__((__packed__)) led_tape_t;
 
 }
+
+struct rgb_t{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
 
 
 static CSType_bool_t UserTask_canCallback(CSReg_t reg, const uint8_t* data, size_t len);
 static void UserTask_resetCallback(void);
 static void UserTask_timerCallback(void);
-static void UserTask_runLED(const std::array<rgb_t, LED_N>* red_arr);
+static void UserTask_runLED(const std::array<rgb_t, LED1_N + LED2_N>* red_arr);
 
-static uint16_t g_dma_buff[(24*LED_N)+50];
+static uint16_t g_dma_buff[(24*(LED1_N + LED2_N))+50];
 static volatile bool g_send_complete = true;
 
-static rgb_t g_rgb_reg;
+static led_tape_t g_rgb_reg[2];
 
 static bool g_rst_flg;
 static CSTimer_t g_out_tim;
@@ -33,9 +51,15 @@ static CSTimer_t g_cmd_tim;
 
 void UserTask_setup(void)
 {
-    g_rgb_reg.r = 0;
-    g_rgb_reg.g = 0;
-    g_rgb_reg.b = 0;
+    g_rgb_reg[0].r = 255;
+    g_rgb_reg[0].g = 255;
+    g_rgb_reg[0].b = 255;
+    g_rgb_reg[0].hz = 2 * 10;
+
+    g_rgb_reg[1].r = 255;
+    g_rgb_reg[1].g = 255;
+    g_rgb_reg[1].b = 255;
+    g_rgb_reg[1].hz = 2 * 10;
 
     g_rst_flg = false;
 
@@ -48,14 +72,50 @@ void UserTask_setup(void)
 void UserTask_loop(void)
 {
     uint32_t ms = CSTimer_getMs(g_out_tim);
-    if(10 < ms)
-    {
-        CSTimer_start(&g_out_tim);
+	if(10 < ms)
+	{
+		CSTimer_start(&g_out_tim);
 
-		std::array<rgb_t, LED_N> red_arr;
-		std::fill(red_arr.begin(), red_arr.end(), g_rgb_reg);
+        rgb_t rgb[2];
+        for(size_t i = 0; i < 2; i++)
+        {
+            float bright_rate;
+            if(g_rgb_reg[i].hz != 0){
+                uint32_t current_ms = CSTimer_getMs(g_cmd_tim);
+                uint32_t order_ms = 10000 / static_cast<uint32_t>(g_rgb_reg[i].hz);
+                if(order_ms < current_ms){
+                    CSTimer_start(&g_cmd_tim);
+                    current_ms = 0;
+                }
+
+                if(current_ms < (order_ms/2)){
+                    bright_rate = current_ms / static_cast<float>(order_ms/2);
+                }else if(current_ms){
+                    current_ms -= (order_ms/2);
+                    bright_rate = 1 - current_ms / static_cast<float>(order_ms/2);
+                }
+            }else{
+                bright_rate = 1;
+            }
+
+            bright_rate *= LED_BRIGHT_RATE;
+            rgb[i].r = static_cast<uint8_t>(g_rgb_reg[i].r * bright_rate);
+            rgb[i].g = static_cast<uint8_t>(g_rgb_reg[i].g * bright_rate);
+            rgb[i].b = static_cast<uint8_t>(g_rgb_reg[i].b * bright_rate);
+        }
+
+		std::array<rgb_t, LED1_N + LED2_N> red_arr;
+        for(size_t i = 0; i < LED1_N+LED2_N; i++)
+        {
+            if(i < LED1_N){
+                red_arr[i] = rgb[0];
+            }else{
+                red_arr[i] = rgb[1];
+            }
+        }
+
 		UserTask_runLED(&red_arr);
-    }
+	}
 
     if(g_rst_flg)
     {
@@ -65,30 +125,17 @@ void UserTask_loop(void)
 
 void UserTask_unsafeLoop(void)
 {
-	uint32_t ms = CSTimer_getMs(g_out_tim);
-	if(10 < ms)
-	{
-		CSTimer_start(&g_out_tim);
+    g_rgb_reg[0].r = 255;
+    g_rgb_reg[0].g = 255;
+    g_rgb_reg[0].b = 255;
+    g_rgb_reg[0].hz = 2 * 10;
 
-		uint32_t cmd_ms = CSTimer_getMs(g_cmd_tim);
-		if(500 < cmd_ms){
-			CSTimer_start(&g_cmd_tim);
-			cmd_ms = 0;
-		}
+    g_rgb_reg[1].r = 255;
+    g_rgb_reg[1].g = 255;
+    g_rgb_reg[1].b = 255;
+    g_rgb_reg[1].hz = 2 * 10;
 
-		uint8_t bright;
-		if(cmd_ms < 250){
-			bright = 255 - static_cast<uint8_t>(cmd_ms * (255.0f / 250.0f));
-		}else if(cmd_ms < 500){
-			cmd_ms -= 250;
-			bright = static_cast<uint8_t>(cmd_ms * (255.0f / 250.0f));
-		}
-
-		rgb_t rgb = {bright, bright, bright};
-		std::array<rgb_t, LED_N> red_arr;
-		std::fill(red_arr.begin(), red_arr.end(), rgb);
-		UserTask_runLED(&red_arr);
-	}
+	UserTask_loop();
 }
 
 static void UserTask_timerCallback(void)
@@ -97,9 +144,14 @@ static void UserTask_timerCallback(void)
 
 static CSType_bool_t UserTask_canCallback(CSReg_t reg, const uint8_t* data, size_t len)
 {
-    if((reg == CSReg_0) && (len == sizeof(rgb_t)))
+    if((reg == CSReg_0) && (len == sizeof(led_tape_t)))
     {
-    	g_rgb_reg = *((const rgb_t*)data);
+    	g_rgb_reg[0] = *((const led_tape_t*)data);
+        return CSTYPE_TRUE;
+    }
+    if((reg == CSReg_1) && (len == sizeof(led_tape_t)))
+    {
+    	g_rgb_reg[1] = *((const led_tape_t*)data);
         return CSTYPE_TRUE;
     }
     return CSTYPE_FALSE;
@@ -110,7 +162,7 @@ static void UserTask_resetCallback(void)
 	g_rst_flg = 1;
 }
 
-static void UserTask_runLED(const std::array<rgb_t, LED_N>* red_arr){
+static void UserTask_runLED(const std::array<rgb_t, LED1_N + LED2_N>* red_arr){
 	if(!g_send_complete){
 		CSLed_err();
 		return;
